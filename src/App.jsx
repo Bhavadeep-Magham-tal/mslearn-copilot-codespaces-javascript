17e546cc97173ee91cf2cf623e905547a177e811
Bhavadeep Magham <Bhavadeep.Magham@talentica.com>
2024-08-17 13:23:30 +0000

    effects added on icons, personal social links updated

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Bhavadeep Magham",
+  title: "Sr Software Engineer",
+  email: "Bhavadeep.Magham@talentica.com",
+  gitHub: "Bhavadeep-Magham-tal",
+  instagram: "ig.m.bs",
+  linkedIn: "m.bhavadeep",
+  medium: "Hey",
+  twitter: "tt_m_b_s",
+  youTube: "Wirally",
 };
 
 const primaryColor = "#4E567E";
